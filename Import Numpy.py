397510e2c2966663b3863600ebac8c3277caecc4
KousarRaza <kousarraza69@gmail.com>
2026-02-25 16:20:17 +0330

    initial updated

---
+++ Import Numpy.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import numpy as np
+
+
+# # Creating a 1 Dimension Array
+
+# In[8]:
+
+
+a=np.array([1,2,3])
+
+
+# In[ ]:
+
+
+a
+
+
+# In[11]:
+
+
+# create an zero array with 3 elements 
+z=np.zeros(3)
+z
+
+
+# In[12]:
+
+
+# create an array with ones 4 elements
+o=np.ones(4)
+o
+
+
+# In[13]:
+
+
+# create an empty array with 5 elements 
+e=np.empty(5)
+
+
+# In[14]:
+
+
+e
+
+
+# In[16]:
+
+
+# with range of elements 
+e=np.arange(6)
+
+
+# In[17]:
+
+
+e
+
+
+# In[20]:
+
+
+a= np.array([1,2,3])
+b=np.ones(3)
+c= np.zeros(4)
+d=np.empty(2)
+a,b,c,d
+
+
+# In[21]:
+
+
+# with specific range of elements
+r=np.arange(2,20)
+r
+
+
+# In[23]:
+
+
+# continue with 2 steps 
+r=np.arange(2,20,2)
+r
+
+
+# In[ ]:
+
+
+
+
